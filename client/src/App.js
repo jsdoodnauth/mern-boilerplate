31a7d87da12ec14f79fb6469b430b92cf08d5b72
Joshua Doodnauth <jsdoodnauth@gmail.com>
2019-04-30 23:55:26 -0400

    fixed logout on page refresh

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(token);
   store.dispatch(setCurrentUser(decoded));
 
-  const currentTime = Date.now();
+  const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
     window.location.href = './login';
